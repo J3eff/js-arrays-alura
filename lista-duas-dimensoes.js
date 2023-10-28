5933e291ad01bf03255f6d305c97626d8133890d
J3eff <jeffersonvideo125@gmail.com>
2023-10-28 16:56:35 -0300

    Lista com 2 dimensões

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedia = [alunos, medias];
+
+console.log(listaDeAlunosEMedia);
+console.log(`O aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedia[0][1]}. A nota dessa aluna é ${listaDeAlunosEMedia[1][1]}`);
