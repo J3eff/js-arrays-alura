08cf701f16b731ba5807804457049eb4b5364dd3
J3eff <jeffersonvideo125@gmail.com>
2023-11-01 00:14:50 -0300

    Somando com reduce

--- media-geral.js
+++ media-geral.js
@@ -3,12 +3,8 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasDaSala) {
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
-        return acumulador + nota;
-    }, 0)
-
+    const somaDasNotas = notasDaSala.reduce((acc, nota) =>  acc + nota, 0);
     const media = somaDasNotas / notasDaSala.length;
-
     return media;
 }
 
